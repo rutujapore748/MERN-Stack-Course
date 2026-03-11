96c263cc59cea07a9deaddeaea045edc058e4431
rutujapore748 <rutujapore748@gmail.com>
2026-03-11 23:10:01 +0530

    done

--- 3 Month March/CRUD Project/Backend/server.js
+++ 3 Month March/CRUD Project/Backend/server.js
@@ -3,3 +3,148 @@
 //Node js
 //Express Js
 //DB MangoDb
+
+
+//API List
+//1.API Create Item - get Data values from frontend(Item Details) and store into DB.
+
+//2.API Update Item - get Item Details from frontend which item need to update.
+
+//3.API Delete Item - get Item Details from frontend and delete this record from dataBase.
+
+//4.API get All Records - get All records From DB and show to UI frontend.
+
+//console.log("Hello Node Js")
+
+
+
+//const getData - () ->{
+
+//}
+
+//function getData(){
+    
+//}
+
+
+console.log("Hello Node Js Project Started")
+
+const express = require('express')    //Node js framework is express js
+const app = express()   // app-variable - store express function
+const mongoose = require('mongoose')   // Liabrary - connect mongodb Database
+const cors = require('cors')   // Liabrary solve cors error
+
+app.use(express.json())     // convert all data into json format
+app.use(cors())      
+
+
+//DB Connection
+
+mongoose.connect("mongodb://localhost:27017/item-database").then(() => console.log("MongoDB Connected") ).catch((error) => console.log("error"))
+
+
+//Schema - Model - Data base Table Structure
+//values store database -structure
+
+
+const itemsSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    purchasePrice: Number,
+    sellingPrice: Number,
+    quantity: Number,
+    unit: String
+})
+
+const Items = new mongoose.model("Items",itemsSchema)  // Table Name  / Collection Name - Items
+
+
+//1.API Create Item
+
+app.post("/api/create-item" , async(req, res) => {
+    try{
+        const { name , description , purchasePrice , sellingPrice ,  quantity ,  unit } = req.body
+
+        const saveItem = new Items(
+            {
+                name,
+                description,
+                purchasePrice,
+                sellingPrice,
+                quantity,
+                unit
+            }
+            
+        )
+
+        await saveItem.save()
+
+        res.status(201).json({message : "Item Created" , data : saveItem})
+
+
+
+    } catch(error) {
+        console.log(error)
+    }
+
+})
+
+
+//2.API Update/ Edit Item
+
+app.put("/api/update-item" , (req, res) => {
+    try{
+
+    } catch(error) {
+        console.log(error)
+    }
+
+})
+
+
+//3.API Delete Item
+
+app.delete("/api/delete-item" ,(req, res) => {
+    try{
+
+    } catch(error) {
+        console.log(error)
+    }
+})
+
+
+//4.API GetAll Item
+
+app.get("/api/get-all-item" , async(req, res) => {
+    try{
+
+        const items = await Items.find()
+
+        res.status(200).json({message : "Get All Item List" , data : items})
+
+    } catch(error) {
+        console.log(error)
+    }
+})
+
+
+
+
+//Health API
+
+app.get("/helth" ,  (req , res) =>{
+    res.status(200).json({ message : "Server is Running" })
+})
+
+//Server Start
+const PORT = 9090
+
+app.listen( PORT , () =>{
+    console.log("Server Started")
+})
+
+
+
+
+
+
--- 3 Month March/CRUD Project/Frontend/vite-project/src/App.jsx
+++ 3 Month March/CRUD Project/Frontend/vite-project/src/App.jsx
@@ -4,140 +4,180 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Table from 'react-bootstrap/Table';
+import { ToastContainer, toast } from 'react-toastify';
+
+
+
 
 import "./style.css"
+import { useEffect, useState } from 'react';
 
 function App() {
 
+  const [itemName, setItemName] = useState()   //Hook - use State Hook
+  const [itemData , setData] = useState()
+
+  console.log(itemName, "Item Name Value");
+
+  const handleOnChange = (event) => {
+
+    setItemName(event.target.value)
+
+    console.log("Typing on Input Field");
+  };
+
+  function SubmitForm(e) {
+    e.preventDefault();
+    console.log("Form Submitted");
+
+
+
+    toast.success('Form Submitted', {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
+
+  const getAllItemData = async () => {
+
+    try {
+
+      const apiResponse = await fetch("http://localhost:9090/api/get-all-item");
+      const responseData = await apiResponse.json();
+      setData(responseData.data);
+
+      console.log(responseData);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getAllItemData();
+  }, []);
+
+  console.log(
+    itemData, "itemData ==>"
+  )
 
   return (
     <>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+
       <h2 className='text-danger text-center my-5'>CRUD MERN STack Project Start</h2>
 
       <div className='container'>
         <div className='row'>
           <div className='col-md-6'>
-            <h3 className='border text-center'>Add Item</h3>
+            <h3 className='border text-center'>Create Item</h3>
 
             <Form className='my-5'>
               <Row className="mb-3">
                 <Form.Group as={Col} controlId="formGridEmail">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Label>Item Name</Form.Label>
+                  <Form.Control type="text" placeholder="Enter Item Name" onChange={() => handleOnChange(event)} />
                 </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
+                <Form.Group as={Col} controlId="formGridZip">
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control type="text" placeholder='Enter Description' />
                 </Form.Group>
               </Row>
 
-              <Form.Group className="mb-3" controlId="formGridAddress1">
-                <Form.Label>Address</Form.Label>
-                <Form.Control placeholder="1234 Main St" />
-              </Form.Group>
+              <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridPassword">
+                  <Form.Label>Purchase Price</Form.Label>
+                  <Form.Control type="number" placeholder="Enter Purchase Price" />
+                </Form.Group>
+                <Form.Group as={Col} controlId="formGridAddress1">
+                  <Form.Label>Selling Price</Form.Label>
+                  <Form.Control type="number" placeholder="Enter Selling Price" />
+                </Form.Group>
+              </Row>
 
-              
               <Row className="mb-3">
                 <Form.Group as={Col} controlId="formGridCity">
-                  <Form.Label>City</Form.Label>
-                  <Form.Control />
+                  <Form.Label>Quantity</Form.Label>
+                  <Form.Control type="number" placeholder='Enter Quantity' />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridState">
-                  <Form.Label>State</Form.Label>
-                  <Form.Select defaultValue="Choose...">
-                    <option>Choose...</option>
-                    <option>...</option>
+                  <Form.Label>Unit</Form.Label>
+                  <Form.Select defaultValue="Choose Unit">
+                    <option>Choose Unit</option>
+                    <option>Piece</option>
+                    <option>Box</option>
+                    <option>Kg</option>
+                    <option>Gram</option>
+                    <option>Litre</option>
                   </Form.Select>
-                </Form.Group>
-
-                <Form.Group as={Col} controlId="formGridZip">
-                  <Form.Label>Zip</Form.Label>
-                  <Form.Control />
                 </Form.Group>
               </Row>
 
               <div className='text-center'>
-              <Button variant="primary" type="submit" className='w-50'>
-                Submit
-              </Button>
+                <Button variant="primary" type="submit" className='w-50' onClick={SubmitForm}>
+                  Submit
+                </Button>
               </div>
             </Form>
 
           </div>
           <div className='col-md-6'>
-            <h3 className='border text-center'>Item List</h3>
+            <h3 className='border text-center'>Get Item</h3>
 
             <Table striped bordered hover>
               <thead>
                 <tr>
                   <th>Id</th>
-                  <th>Email</th>
-                  <th>Password</th>
-                  <th>Address</th>
-                  <th>City</th>
-                  <th>State</th>
-                  <th>Zip</th>
+                  <th>Item Name</th>
+                  <th>Description</th>
+                  <th>Purchase Price</th>
+                  <th>Selling Price</th>
+                  <th>Quantity</th>
+                  <th>Unit</th>
+                  <th>Action</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>test@gmail.com</td>
-                  <td>123</td>
-                  <td>Karad</td>
-                  <td>Karad</td>
-                  <td>Maharashtra</td>
-                  <td>415120</td>
-                </tr>
-                <tr>
-                  <td>1</td>
-                  <td>test@gmail.com</td>
-                  <td>123</td>
-                  <td>Karad</td>
-                  <td>Karad</td>
-                  <td>Maharashtra</td>
-                  <td>415120</td>
-                </tr>
-                <tr>
-                  <td>1</td>
-                  <td>test@gmail.com</td>
-                  <td>123</td>
-                  <td>Karad</td>
-                  <td>Karad</td>
-                  <td>Maharashtra</td>
-                  <td>415120</td>
-                </tr>
-                <tr>
-                  <td>1</td>
-                  <td>test@gmail.com</td>
-                  <td>123</td>
-                  <td>Karad</td>
-                  <td>Karad</td>
-                  <td>Maharashtra</td>
-                  <td>415120</td>
-                </tr>
-                <tr>
-                  <td>1</td>
-                  <td>test@gmail.com</td>
-                  <td>123</td>
-                  <td>Karad</td>
-                  <td>Karad</td>
-                  <td>Maharashtra</td>
-                  <td>415120</td>
-                </tr>
-                <tr>
-                  <td>1</td>
-                  <td>test@gmail.com</td>
-                  <td>123</td>
-                  <td>Karad</td>
-                  <td>Karad</td>
-                  <td>Maharashtra</td>
-                  <td>415120</td>
-                </tr>
-                
-                
+                  {
+                    itemData && itemData.map((each , index) =>{
+
+                      return(
+                        <tr>
+                          <td>{index + 1}</td>
+                          <td>{each.name}</td>
+                          <td>{each.description}</td>
+                          <td>{each.purchasePrice}</td>
+                          <td>{each.sellingPrice}</td>
+                          <td>{each.quantity}</td>
+                          <td>{each.unit}</td>
+                          <td className='d-flex'>
+                            <button className='btn btn-sucess'>Edit</button>
+                            <button className='btn btn-danger mx-2'>Delet</button>
+                          </td>
+                        </tr>
+                      )
+
+                    })
+                  }
               </tbody>
             </Table>
           </div>
